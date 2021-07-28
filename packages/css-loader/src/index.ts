32e795c305b62d56fb162ebc1ae8a4c31285f3db
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-07-28 19:14:18 +0300

    refactor(loader): move loader to file

--- packages/css-loader/src/index.ts
+++ packages/css-loader/src/index.ts
@@ -1,22 +1,6 @@
-import { transform } from './transform';
 import { appendStyle } from "./append-style";
-import type { ExtendedLoaderContext } from './types';
+import { cssLoader } from './loader'
 
-export * from './types'
-export { appendStyle }
-
-export default function cssLoader(this: ExtendedLoaderContext<any>, content: string): string {
-  let injectCssInJs = true
-  const { classesMap, css } = transform(this.resourcePath, content)
-
-  if (this.setOutputCSS) {
-    injectCssInJs = false
-    this.setOutputCSS(css)
-  }
-
-  const x = `${injectCssInJs ? `(${appendStyle})(${JSON.stringify(css)})` : ''}
-
-  export default ${JSON.stringify(Object.fromEntries(classesMap.entries()))}`
-
-  return x
-}
+export * from './types';
+export { appendStyle };
+export default cssLoader
---
+++ packages/css-loader/src/loader.ts
@@ -0,0 +1,19 @@
+import { transform } from './transform';
+import { appendStyle } from "./append-style";
+import type { ExtendedLoaderContext } from './types';
+
+export function cssLoader(this: ExtendedLoaderContext<any>, content: string): string {
+  let injectCssInJs = true
+  const { classesMap, css } = transform(this.resourcePath, content)
+
+  if (this.setOutputCSS) {
+    injectCssInJs = false
+    this.setOutputCSS(css)
+  }
+
+  const x = `${injectCssInJs ? `(${appendStyle})(${JSON.stringify(css)})` : ''}
+
+  export default ${JSON.stringify(Object.fromEntries(classesMap.entries()))}`
+
+  return x
+}
